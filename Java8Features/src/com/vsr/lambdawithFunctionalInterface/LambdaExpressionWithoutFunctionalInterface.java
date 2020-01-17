09875a696ed4c754353a2f678a569c518d069349
reddys11 <vakasantoshreddy@gmail.com>
2020-01-17 12:33:21 +0530

    First Commit

---
+++ Java8Features/src/com/vsr/lambdawithFunctionalInterface/LambdaExpressionWithoutFunctionalInterface.java
@@ -0,0 +1,9 @@
+package com.vsr.lambdawithFunctionalInterface;
+
+import com.vsr.normalInterface.Caluculator;
+
+class LambdaExpressionWithoutFunctionalInterface {
+public static void main(String[] args){
+	Caluculator c= (a,b)->System.out.println(a+b);
+}
+}
---
+++ Java8Features/src/com/vsr/normalInterface/Caluculator.java
@@ -0,0 +1,6 @@
+package com.vsr.normalInterface;
+@FunctionalInterface
+public interface Caluculator {
+		public void add(int a,int b);
+		//public void sub(int a,int b);
+}
